14b2f455bf2fc6f9f03957a0a7421eb17f8c49d6
sirius <95621941@qq.com>
2026-07-28 14:25:02 +0800

    feat: install governed Node change Ops upgrade scope

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -24,6 +24,7 @@ def load_authorized_upgrade() -> ModuleType:
 
     from .node_upgrade_policy import install as install_node_policy
     from .read_ops_upgrade_policy import install as install_read_ops_policy
+    from .change_ops_upgrade_policy import install as install_change_ops_policy
     from .repair_upgrade_policy import install as install_repair_policy
     from .upgrade_redlines import install as install_diff_redlines
 
@@ -32,6 +33,7 @@ def load_authorized_upgrade() -> ModuleType:
     install_node_policy()
     module.NODE_UPGRADE_POLICY_VERSION = "owner-authorized-node-upgrade-v1"
     install_read_ops_policy(module)
+    install_change_ops_policy(module)
     install_repair_policy(module)
     install_diff_redlines(module)
 
